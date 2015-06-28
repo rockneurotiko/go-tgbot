09cdffc041bb44670a789a0f494efe0c30d01313
Rock Neurotiko <miguelglafuente@gmail.com>
2015-06-28 14:40:29 +0200

    Echo example

---
+++ example/echoexample/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rockneurotiko/go-tgbot"
+)
+
+func echoHandler(bot tgbot.TgBot, msg tgbot.Message, vals []string, kvals map[string]string) *string {
+	newmsg := fmt.Sprintf("[Echoed]: %s", vals[1])
+	return &newmsg
+}
+
+func main() {
+	bot := tgbot.NewTgBot("token").
+		CommandFn(`/echo (.+)`, echoHandler)
+	bot.SimpleStart()
+}
